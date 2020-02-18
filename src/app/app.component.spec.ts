13c5180f0ccf4e8061d96ab1e5dd1ef6cb641068
C1293361 Luan Sousa Santana <c1293361@interno.bb.com.br>
2020-02-17 21:10:33 -0300

    ajustes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,9 @@
 import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
       declarations: [
         AppComponent
       ],
@@ -20,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'blank-www'`, () => {
+  it(`should have as title 'jwt-auth'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('blank-www');
+    expect(app.title).toEqual('jwt-auth');
   });
 
-  it('should render title', () => {
+  it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('blank-www app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to jwt-auth!');
   });
 });
